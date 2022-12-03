8a9245b1d24293663699b5c3f9df8161d1b05676
xlsama <im.xlsama@gmail.com>
2022-12-03 10:27:54 +0800

    refactor: token function

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,7 @@
 import { version } from '../package.json'
 import { program } from 'commander'
 import { uploadImage } from './upload'
-import { readFileSync, writeFileSync } from 'node:fs'
-import { homedir } from 'node:os'
-import path from 'node:path'
-
-const TOKEN_PATH = path.resolve(homedir(), '.config', 'upload-image.json')
+import { getTokenFromLocal, setTokenToLocal } from './token'
 
 program.version(version)
 
@@ -18,23 +14,8 @@ program.parse()
 const { token } = program.opts()
 
 if (token) {
-  setTokenToLocal()
+  setTokenToLocal(token)
 } else {
   const token = getTokenFromLocal()
   token && uploadImage(token)
-}
-
-function setTokenToLocal() {
-  writeFileSync(TOKEN_PATH, JSON.stringify({ token }))
-}
-
-function getTokenFromLocal(): string | undefined {
-  try {
-    const { token } = JSON.parse(
-      readFileSync(TOKEN_PATH, { encoding: 'utf-8', flag: 'a+' })
-    )
-    return token
-  } catch (err) {
-    console.log('token does not exist, you can run "ui --help"')
-  }
 }
---
+++ src/token.ts
@@ -0,0 +1,20 @@
+import { readFileSync, writeFileSync } from 'node:fs'
+import { homedir } from 'node:os'
+import path from 'node:path'
+
+const TOKEN_PATH = path.resolve(homedir(), '.config', 'upload-image.json')
+
+export function setTokenToLocal(token: string) {
+  writeFileSync(TOKEN_PATH, JSON.stringify({ token }))
+}
+
+export function getTokenFromLocal(): string | undefined {
+  try {
+    const { token } = JSON.parse(
+      readFileSync(TOKEN_PATH, { encoding: 'utf-8', flag: 'a+' })
+    )
+    return token
+  } catch (err) {
+    console.log('token does not exist, you can run "ui --help"')
+  }
+}
